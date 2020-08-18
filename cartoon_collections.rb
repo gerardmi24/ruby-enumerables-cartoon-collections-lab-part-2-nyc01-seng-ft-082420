aa8874e72bf5056d793b3f6060f9f0d99c9297ed
Gerard Mistretta <gerardmistretta@yahoo.com>
2020-08-18 02:04:27 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -30,7 +30,7 @@ def find_valid_calls(planeteer_calls)
   # Return the first valid call found, or return nil if no valid calls are found
   planeteer_calls.find do |element|
     if element = valid_calls
-    puts "valid_calls"
+    puts "#{valid_calls}"
     end
 end
 end
