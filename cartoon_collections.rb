5a7fc95285c30d0a8e14445aa11e168708a41383
Gerard Mistretta <gerardmistretta@yahoo.com>
2020-08-18 01:05:10 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -21,7 +21,7 @@ def long_planeteer_calls(planeteer_calls)
   # Return the boolean result of this check
 planeteer_calls.each do |element|
   if element.length > 4
-    true
+    puts true
 end
 end
 planeteer_calls
